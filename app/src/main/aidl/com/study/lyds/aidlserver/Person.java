cf6a3828e4aee22d7d3031631ae904da1022bacd
chlseeky <2540757573@qq.com>
2018-04-25 01:32:25 +0800

    添加对象

---
+++ app/src/main/aidl/com/study/lyds/aidlserver/Person.java
@@ -0,0 +1,68 @@
+package com.study.lyds.aidlserver;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Administrator on 2018/4/25.
+ */
+
+public class Person implements Parcelable {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    protected Person(Parcel in) {
+        name = in.readString();
+        age = in.readInt();
+    }
+    public static final Creator<Person> CREATOR = new Creator<Person>() {
+        @Override
+        public Person createFromParcel(Parcel in) {
+            return new Person(in);
+        }
+        @Override
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public void readFromParcel(Parcel dest) {
+        name = dest.readString();
+        age = dest.readInt();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
