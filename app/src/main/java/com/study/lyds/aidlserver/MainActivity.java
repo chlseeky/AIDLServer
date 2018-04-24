9938fe7d9d95809b48999cdf897cc30b748cca83
chlseeky <2540757573@qq.com>
2018-04-25 01:52:06 +0800

    change bug

--- app/src/main/java/com/study/lyds/aidlserver/MainActivity.java
+++ app/src/main/java/com/study/lyds/aidlserver/MainActivity.java
@@ -3,6 +3,9 @@ package com.study.lyds.aidlserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * CSDN：https://blog.csdn.net/liuyonglei1314/article/details/54317902
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
